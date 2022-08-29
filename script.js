e02841e638bd023424ae0dbb31d8407319e20677
Laur <laur_laurentiu2013@yahoo.com>
2022-08-29 19:25:21 +0300

    swap home and game pages through loading screen

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
-const loadingScreenDisplay = (() =>{
+"use strict";
+
+const swapScreens = (() =>{
+
     const playBtn = document.querySelector('[data-play]');
     const homePage = document.querySelector('[data-home]');
-    const loadPage = document.querySelector('[data-load]')
+    const loadPage = document.querySelector('[data-load]');
+    const gameScreen = document.querySelector('[data-game]');
+
+
+    function showGameScreen(){
+        loadPage.style.display = 'none';
+        gameScreen.style.display = 'grid';  
+    };
+
     playBtn.addEventListener('click', () => {
         homePage.style.display = 'none';
         loadPage.style.display = 'grid';
-    });
+        setTimeout(showGameScreen, 3500)
+    })
+
 })();
