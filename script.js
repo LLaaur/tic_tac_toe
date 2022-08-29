24e12e49d960d59c91265291bad0e8e65b97f096
Laur <laur_laurentiu2013@yahoo.com>
2022-08-29 21:04:58 +0300

    interface styling for game page

--- script.js
+++ script.js
@@ -2,21 +2,43 @@
 
 const swapScreens = (() =>{
 
-    const playBtn = document.querySelector('[data-play]');
-    const homePage = document.querySelector('[data-home]');
     const loadPage = document.querySelector('[data-load]');
     const gameScreen = document.querySelector('[data-game]');
 
-
     function showGameScreen(){
         loadPage.style.display = 'none';
-        gameScreen.style.display = 'grid';  
+        gameScreen.style.display = 'grid';
     };
+
+    const turn = document.querySelector('[data-turn]');
+    
+    if (gameScreen){
+        turn.textContent = 'Player\'s X turn'
+    }
+
+    const playBtn = document.querySelector('[data-play]');
+    const homePage = document.querySelector('[data-home]');
 
     playBtn.addEventListener('click', () => {
         homePage.style.display = 'none';
         loadPage.style.display = 'grid';
         setTimeout(showGameScreen, 3500)
+    })
+
+
+})();
+
+const gameBoard = (() => {
+    const board = ['','','','','','','','',''];
+
+    const allBoardSquares = document.querySelectorAll('[data-square]');
+
+    const restartBtn = document.querySelector('[data-restart]');
+
+    restartBtn.addEventListener('click', () => {
+        allBoardSquares.forEach(
+            square => square.textContent = ''
+        )
     })
 
 })();
